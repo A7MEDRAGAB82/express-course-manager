f8276ef25cbe70020dc1611d9536166f110627a8
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2025-12-27 23:32:49 +0200

    add course model

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require("express");
 
 const app = express();
 
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb+srv://AgmadRagab:learn2412@learn-mongodb.djiozux.mongodb.net/<codeZone>?appName=learn-mongodb";
+
+mongoose.connect(url).then(() => {
+  console.log("mongodb server started");
+});
+
 app.use(express.json());
 
-const coursesRouter = require('./routes/courses.route')
+const coursesRouter = require("./routes/courses.route");
 
-app.use('/api/courses',coursesRouter)
-
-
+app.use("/api/courses", coursesRouter);
 
 app.listen(4000, () => {
   console.log("listening on port: 4000");
 });
-
