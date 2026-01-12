b9cf78c47baf92028228d4385874d2fb918e3c57
Ahmed Ragab <sci.AhmedRagab85728@alexu.edu.eg>
2026-01-12 20:50:53 +0200

    add CORS

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 
-const httpStatusText = require('./utils/httpStatusText')
+const httpStatusText = require("./utils/httpStatusText");
 
 const app = express();
 
-
-
-
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
 
-
+app.use(cors())
 app.use(express.json());
 
 const coursesRouter = require("./routes/courses.route");
@@ -23,9 +22,10 @@ const coursesRouter = require("./routes/courses.route");
 app.use("/api/courses", coursesRouter);
 
 app.use((req, res, next) => {
-    res.status(404).json({status: httpStatusText.ERROR, message: "Resource not found"});
+  res
+    .status(404)
+    .json({ status: httpStatusText.ERROR, message: "Resource not found" });
 });
-
 
 app.listen(4000, () => {
   console.log("listening on port: 4000");
